d36cfc5336a735782b8ecce640eee6395f662a89
Àngel Mariages <angel.mariage@tribescale.com>
2017-03-27 12:53:43 +0200

    Add fibonacci class

---
+++ src/m07junitdemo3/Fibonacci.java
@@ -0,0 +1,16 @@
+package m07junitdemo3;
+
+/**
+ * @author manuel y angel
+ */
+
+public class Fibonacci {
+	public static int compute(int n) {
+		int result;
+
+		if (n <= 1) result = n;
+		else result = compute(n - 1) + compute(n - 2);
+
+		return result;
+	}
+}
